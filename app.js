9498df4ac74d59378b5ad98b84db6fc2d7043cd6
michaldobiezynski <dobiezynski@gmail.com>
2020-05-24 11:59:17 +0100

    improved app.js file

--- app.js
+++ app.js
@@ -1,30 +1,35 @@
+// Main starting point of the application
 const express = require("express");
+const http = require("http");
 const bodyParser = require("body-parser");
-const mongoose = require("mongoose");
-
+const morgan = require("morgan");
 const app = express();
+const router = require("./router");
+const mongoose = require("mongoose");
+const cors = require("cors");
 
 app.use(bodyParser.json());
 
-app.use((req, res, next) => {
-  res.setHeader("Access-Control-Allow-Origin", "*");
-  res.setHeader(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept, Authorization",
-  );
-  res.setHeader("Access-Control-Allow-Methods", "GET, POST, PATCH, DELETE");
+// App Setup
+app.use(morgan("combined"));
+app.use(cors());
+app.use(bodyParser.json({ type: "*/*" }));
+router(app);
 
-  next();
-});
-
+// DB Setup
 mongoose
   .connect(
     `mongodb+srv://michal123:michal123@devconnector-6tf9d.mongodb.net/MiloOfCroton?retryWrites=true&w=majority`,
     { useNewUrlParser: true, useUnifiedTopology: true },
   )
   .then(() => {
-    app.listen(process.env.PORT || 5000);
-    console.log("API started");
+    console.log("MongoDB connected");
+
+    // Server Setup
+    const port = process.env.PORT || 5000;
+    const server = http.createServer(app);
+    server.listen(port);
+    console.log("Server listening on:", port);
   })
   .catch((err) => {
     console.log(err);
