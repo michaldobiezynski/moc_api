a7fad6a35af17f0bb0fa67eb869163c0d3fc4fea
michaldobiezynski <dobiezynski@gmail.com>
2020-05-25 16:56:53 +0100

    fixedo typo

--- routes/users.js
+++ routes/users.js
@@ -6,7 +6,7 @@ const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
 const config = require("config");
 
-const User = require("../models/User");
+const User = require("../models/user");
 
 // @route   POST api/users
 // @desc    Register user
