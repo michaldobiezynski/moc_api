b24652fdb61cab4f193d32196a6aec7540e366c3
michaldobiezynski <dobiezynski@gmail.com>
2020-05-25 16:12:02 +0100

    added dependencies and created users route

--- app.js
+++ app.js
@@ -12,7 +12,12 @@ connectDB();
 // App Setup
 app.use(cors());
 app.use(express.json({ extended: false }));
+
+// Define Routes
 app.get("/", (req, res) => res.send("API Running"));
+
+app.use("/api/users", require("./routes/api/users"));
+app.use("/api/auth", require("./routes/api/auth"));
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
