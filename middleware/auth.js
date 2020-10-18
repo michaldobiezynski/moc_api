f35267aaa518dd9cfa7c42f2504111a5120fd440
michaldobiezynski <michaldobiezynski@hotmail.com>
2020-10-18 18:05:55 +0100

    update auth middleware like I've done it in task manager app

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,22 +1,24 @@
-const jwt = require('jsonwebtoken');
-const config = require('config');
+const jwt = require("jsonwebtoken");
 
-module.exports = function(req, res, next) {
-  //Get token from header
-  const token = req.header('x-auth-token');
+const User = require("../models/User");
 
-  // Check if not token
-  if (!token) {
-    return res.status(401).json({ msg: 'No token, authorization denied' });
-  }
-
-  //Verify token
+const auth = async (req, res, next) => {
   try {
-    const decoded = jwt.verify(token, config.get('jwtSecret'));
-
-    req.user = decoded.user;
+    const token = req.header("Authorization").replace("Bearer ", "");
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+    const user = await User.findOne({
+      _id: decoded._id,
+      "tokens.token": token,
+    });
+    if (!user) {
+      throw new Error();
+    }
+    req.token = token;
+    req.user = user;
     next();
   } catch (error) {
-    res.status(401).json({ msg: 'Token is not valid.' });
+    res.status(401).send({ error: "Please authenticate" });
   }
 };
+
+module.exports = auth;
