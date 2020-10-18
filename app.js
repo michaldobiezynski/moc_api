6077decb274d63b5dfcd18534c11560cce1b35d4
michaldobiezynski <michaldobiezynski@hotmail.com>
2020-10-18 19:18:48 +0100

    move db config to it's onw folder and fix the name of the user routes in app

--- app.js
+++ app.js
@@ -2,7 +2,7 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const cors = require("cors");
-const connectDB = require("./config/db");
+const connectDB = require("./db/db");
 
 const app = express();
 
@@ -16,7 +16,7 @@ app.use(express.json({ extended: false }));
 // Define Routes
 app.get("/", (req, res) => res.send("API Running"));
 
-app.use("/api/users", require("./routes/users"));
+app.use(require("./routes/users"));
 app.use("/api/workouts", require("./routes/workout"));
 
 const PORT = process.env.PORT || 5000;
