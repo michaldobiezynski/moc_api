d7dcb1c8b3b79f8daf226476566d3cd00821bf3c
michaldobiezynski <michaldobiezynski@hotmail.com>
2020-12-01 19:32:53 +0000

    add try and catch to sending email functionality

--- emails/account.js
+++ emails/account.js
@@ -3,22 +3,30 @@ const sgMail = require("@sendgrid/mail");
 sgMail.setApiKey(process.env.SENDGRID_API_KEY);
 
 const sendWelcomeEmail = (email, name) => {
-  sgMail.send({
-    to: email,
-    from: "milo_of_croton@outlook.com",
-    subject: "Thanks for joining in!",
-    text: `Welcome to the app, ${name}. Let me know how you get along with the app`,
-  });
+  try {
+    sgMail.send({
+      to: email,
+      from: "milo_of_croton@outlook.com",
+      subject: "Thanks for joining in!",
+      text: `Welcome to the app, ${name}. Let me know how you get along with the app`,
+    });
+  } catch (error) {
+    console.log(error);
+  }
 };
 const sendDeleteEmail = (email, name) => {
-  sgMail.send({
-    to: email,
-    from: "milo_of_croton@outlook.com",
-    subject: "Sorry to see you go!",
-    text:
-      `Hi ${name}, I was wondering why you deleted your account and if there is` +
-      ` anything I could do to keep you`,
-  });
+  try {
+    sgMail.send({
+      to: email,
+      from: "milo_of_croton@outlook.com",
+      subject: "Sorry to see you go!",
+      text:
+        `Hi ${name}, I was wondering why you deleted your account and if there is` +
+        ` anything I could do to keep you`,
+    });
+  } catch (error) {
+    console.log(error);
+  }
 };
 
 module.exports = {
