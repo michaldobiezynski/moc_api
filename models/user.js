aab181647f9053752377db9ad8bb98cd66b91120
michaldobiezynski <michaldobiezynski@hotmail.com>
2020-12-02 13:10:05 +0000

    create function in user schema for generating temp token

--- models/user.js
+++ models/user.js
@@ -87,6 +87,17 @@ userSchema.methods.generateAuthToken = async function () {
 
   return token;
 };
+userSchema.methods.generateTempAuthToken = async function () {
+  const user = this;
+  const token = jwt.sign({ _id: user._id.toString() }, process.env.JWT_SECRET, {
+    expiresIn: 1,
+  });
+
+  user.tokens = user.tokens.concat({ token });
+  await user.save();
+
+  return token;
+};
 
 userSchema.statics.findByCredentials = async (email, password) => {
   const user = await User.findOne({ email });
