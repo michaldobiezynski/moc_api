4a76d7f0167a77f1e3de752df473bc229e42cb02
michaldobiezynski <michaldobiezynski@hotmail.com>
2020-12-02 13:30:57 +0000

    finish up password reset endpoint

--- routes/users.js
+++ routes/users.js
@@ -6,17 +6,13 @@ const { sendWelcomeEmail, sendDeleteEmail } = require("../emails/account");
 
 const router = new express.Router();
 
-
-
 router.post("/passwordReset", async (req, res) => {
-  const user = new User(req.body);
   try {
-    await user.save();
-    sendWelcomeEmail(user.email, user.name);
+    const user = await User.findByEmail(req.body.email);
     const token = await user.generateAuthToken();
-    res.status(201).send({ user, token });
+    res.status(200).send({ user, token });
   } catch (error) {
-    res.status(400).send(error);
+    res.status(400).send();
   }
 });
 
